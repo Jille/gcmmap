63ce0a539a9120e77c26e37e1cfb234f98062e37
Jille Timmermans <jille@quis.cx>
2024-09-06 09:45:46 +0200

    Allocate the bytes container without zeroing
    Skipping the useless zeroing is a significant performance boost,
    especially if large regions are mmapped.
    
    It also increases this library's evil points, so why not.

--- gcmmap.go
+++ gcmmap.go
@@ -22,8 +22,8 @@ var NumActive atomic.Int32
 func Mmap(fd int, offset int64, len, prot, flags int) ([]byte, error) {
 	// Do a regular allocation, which we can set a finalizer on.
 	// Make it slightly larger than the requested length, to allow us to align to a page boundary, and to make sure we don't mmap over another allocation on the same page.
-	container := make([]byte, (len+4096+4095) & ^4095)
-	pageStart, skip := alignPointer(unsafe.Pointer(unsafe.SliceData(container)))
+	container := allocateDirtyBytes((len + 4096 + 4095) & ^4095)
+	pageStart := alignPointer(container)
 
 	addr, err := unix.MmapPtr(fd, offset, pageStart, uintptr(len), prot, flags|unix.MAP_FIXED)
 	if err != nil {
@@ -34,8 +34,8 @@ func Mmap(fd int, offset int64, len, prot, flags int) ([]byte, error) {
 	}
 	NumActive.Add(1)
 
-	runtime.SetFinalizer(unsafe.SliceData(container), func(c *byte) {
-		pageStart, _ := alignPointer(unsafe.Pointer(c))
+	runtime.SetFinalizer((*byte)(container), func(container *byte) {
+		pageStart := alignPointer(unsafe.Pointer(container))
 		addr, err := unix.MmapPtr(-1, 0, pageStart, uintptr(len), unix.PROT_READ|unix.PROT_WRITE, unix.MAP_ANONYMOUS|unix.MAP_PRIVATE|unix.MAP_FIXED)
 		if err != nil {
 			panic(fmt.Errorf("Restoring normal memory failed when gcmmap got dereferenced: %w", err))
@@ -46,13 +46,21 @@ func Mmap(fd int, offset int64, len, prot, flags int) ([]byte, error) {
 		NumActive.Add(-1)
 	})
 
-	return container[skip:][:len], nil
+	return unsafe.Slice((*byte)(pageStart), len), nil
 }
 
-func alignPointer(p unsafe.Pointer) (unsafe.Pointer, int) {
+func alignPointer(p unsafe.Pointer) unsafe.Pointer {
 	o := uintptr(p) % 4096
 	if o == 0 {
-		return p, 0
+		return p
 	}
-	return unsafe.Add(p, 4096-o), 4096 - int(o)
+	return unsafe.Add(p, 4096-o)
 }
+
+// allocateDirtyBytes allocates bytes without zeroing them.
+func allocateDirtyBytes(n int) unsafe.Pointer {
+	return mallocgc(uintptr(n), nil, false)
+}
+
+//go:linkname mallocgc runtime.mallocgc
+func mallocgc(size uintptr, typ unsafe.Pointer, needzero bool) unsafe.Pointer
